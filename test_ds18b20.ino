1c433a07741fb06eae56e03389c487b0bba7cd5f
Kyuho Kim <valve.god.project@gmail.com>
2017-11-22 18:44:00 +0900

    Create test_ds18b20.ino

---
+++ test_ds18b20.ino
@@ -0,0 +1,20 @@
+//
+//    FILE: test_ds18b20.ino
+//  AUTHOR: Kyuho Kim (ekyuho@gmail.com)
+// CREATED: November 22, 2017
+// Released to the public domain
+//
+#include <SoftwareSerial.h>
+SoftwareSerial dust(D1, D0, false, 256);
+
+void setup() {
+  Serial.begin(115200);
+  dust.begin(9600);
+
+  Serial.println("\nReading dust sensors");
+}
+
+void loop() {
+  while (dust.available() > 0)
+    Serial.println(dust.read(), HEX);
+}
